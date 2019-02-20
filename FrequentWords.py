04f6294cac9eec2e1068a5bdf7966ad68e7f73ba
nrezaie <nrezaie@ce.sharif.edu>
2019-02-20 12:20:34 +0330

    Add FrequentWords

---
+++ FrequentWords.py
@@ -0,0 +1,22 @@
+# Input:  A string Text and an integer k
+# Output: A list containing all most frequent k-mers in Text
+def FrequentWords(Text, k):
+    words = []
+    freq = FrequencyMap(Text, k)
+    m = max(freq.values())
+    for key in freq:
+        if freq[key] == m:
+            words.append(key)
+    return words
+
+# Copy your FrequencyMap() function here.
+def FrequencyMap(Text, k):
+    freq = {}
+    n = len(Text)
+    for i in range(n-k+1):
+        Pattern = Text[i:i+k]
+        freq[Pattern] = 0
+        for i in range(n-k+1):
+            if Text[i:i+k] == Pattern:
+                freq[Pattern] = freq[Pattern] + 1
+    return freq
